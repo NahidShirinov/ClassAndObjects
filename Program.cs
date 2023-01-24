b1acd4364d29b14e04023db1db331c137ad509db
NahidShirinov <nahidsrnv50@gmail.com>
2023-01-24 15:03:48 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    Console.WriteLine("Hello, World!");
+            //    triangle triangle = new triangle(23, 3, 44);
+            //    Console.WriteLine(triangle.perimetr());
+            //}
+
+
+            Coordinenatr coordinenatr= new Coordinenatr(3,5,8);
+            Console.WriteLine(coordinenatr.pemetr());
+            }
+    }
+}
---
+++ triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjects
+{
+    internal class triangle
+    {
+
+    }
+
+    struct Coordinenatr
+    {
+        public int en;
+        public int uzunlug1;
+        public int uzunlug2;
+        public int pemetr()
+        {
+            return (uzunlug1 + uzunlug2 + en);
+        }
+
+        public Coordinenatr (int en, int uzunlug1, int uzunlug2)
+        {
+            this.en = en;
+            this.uzunlug1 = uzunlug1;
+            this.uzunlug2 = uzunlug2;
+            
+
+        }
+    }
+}
